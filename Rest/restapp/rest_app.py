42373b6709604757595e8797a44adfe6992dae1e
aniruddha674 <aniruddhapatil674@gmail.com>
2022-08-31 22:42:11 +0530

    Updated

---
+++ Rest/restapp/rest_app.py
@@ -0,0 +1,32 @@
+from flask import Flask ,request
+from flask_restx import Resource ,Api ,fields
+import datetime
+
+app = Flask('__name__')
+api = Api(app)
+
+model1= api.model('model1', {
+    'Timestamp': fields.String,
+    'Temperature': fields.Integer(min=0) ,
+    'Note': fields.String
+})
+
+
+@api.route('/Temp/<int:Temperature>,<Note>',endpoint='/Temp')
+@api.doc(params={"Temperature":"Put current Temperature","Note":"Input note if any"})
+class MyResource(Resource):
+    @api.doc(model=model1)
+    def post(self,Temperature,Note):
+        x=datetime.datetime.now()
+        x=str(x)
+        return {"Timestamp":x,"Temperature":"{0}°C".format(Temperature),"Note":Note}
+
+# @api.route('/hello')
+# class HelloWorld(Resource):
+#     def get(self):
+#         return {'hello': 'world'}
+########################################################
+
+
+if __name__ =='__main__':
+    app.run(debug=True, host="0.0.0.0")
--- Rest/venv/Scripts/activate.bat
+++ Rest/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Celebal\Current\Rest\venv
+set VIRTUAL_ENV=E:\Celebal\Current\Projects\Rest\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
